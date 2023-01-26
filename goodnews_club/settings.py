b875371d5bcc1364fe2d2aa1d42cd85c8d4b2101
Michaelizzy1 <michaelizzy2@gmail.com>
2023-01-26 19:01:52 +0100

    dd

--- goodnews_club/settings.py
+++ goodnews_club/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'goodnews_club.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'jC3eekMs1jkfTdZr33C8',
+        'HOST': 'containers-us-west-163.railway.app',
+        'PORT': '6723',
     }
 }
 
